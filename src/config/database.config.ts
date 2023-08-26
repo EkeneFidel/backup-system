330097f609139ace9091849472f35e3eb53a80f9
EkeneFidel <echukwurah99@gmail.com>
2023-08-26 03:11:19 +0100

    feat: docker settings

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -14,23 +14,32 @@ class Database {
   private POSTGRES_PORT = process.env.POSTGRES_PORT as unknown as number;
   private POSTGRES_USER = process.env.POSTGRES_USER as string;
   private POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD as string;
+  private NODE_ENV = process.env.NODE_ENV as string;
+  private DATABASE_URL = process.env.DATABASE_URL as string;
 
   constructor() {
     this.connectToPostgreSQL();
   }
 
   private async connectToPostgreSQL() {
-    this.sequelize = new Sequelize({
-      database: this.POSTGRES_DB,
-      username: this.POSTGRES_USER,
-      password: this.POSTGRES_PASSWORD,
-      host: this.POSTGRES_HOST,
-      port: this.POSTGRES_PORT,
-      dialect: "postgres",
-      logging: false,
-      models: [User, Folder, File],
-    });
-
+    if (this.NODE_ENV === "production") {
+      this.sequelize = new Sequelize(this.DATABASE_URL, {
+        dialect: "postgres",
+        logging: false,
+        models: [User, Folder, File],
+      });
+    } else {
+      this.sequelize = new Sequelize({
+        database: this.POSTGRES_DB,
+        username: this.POSTGRES_USER,
+        password: this.POSTGRES_PASSWORD,
+        host: this.POSTGRES_HOST,
+        port: this.POSTGRES_PORT,
+        dialect: "postgres",
+        logging: false,
+        models: [User, Folder, File],
+      });
+    }
     await this.sequelize
       .authenticate()
       .then(() => {
